2c634ca02dd91dcc2edab6b4f20dfac3235caeb9
ChandanT21 <vichandan@me.com>
2023-07-18 10:03:38 -0400

    Initial commit

---
+++ display.py
@@ -0,0 +1,37 @@
+logo = """
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+"""
+
+the_deck = [("Ace of Hearts", 11, "ace-of-hearts.png"), ("2 of Hearts", 2, "two-of-hearts.png"), \
+("3 of Hearts", 3, "three-of-hearts.png"), ("4 of Hearts", 4, "four-of-hearts.png"), \
+("5 of Hearts", 5, "five-of-hearts.png"), ("6 of Hearts", 6, "six-of-hearts.png"), \
+("7 of Hearts", 7, "seven-of-hearts.png"), ("8 of Hearts", 8, "eight-of-hearts.png"), \
+("9 of Hearts", 9, "nine-of-hearts.png"), ("10 of Hearts", 10, "ten-of-hearts.png"), \
+("Jack of Hearts", 10, "jack-of-hearts.png"), ("Queen of Hearts", 10, "queen-of-hearts.png"), \
+("King of Hearts", 10, "king-of-hearts.png"), ("Ace of Spades", 11, "ace-of-spades.png"), \
+("2 of Spades", 2, "two-of-spades.png"), ("3 of Spades", 3, "three-of-spades.png"), \
+("4 of Spades", 4, "four-of-spades.png"), ("5 of Spades", 5, "five-of-spades.png"), \
+("6 of Spades", 6, "six-of-spades.png"), ("7 of Spades", 7, "seven-of-spades.png"), \
+("8 of Spades", 8, "eight-of-spades.png"), ("9 of Spades", 9, "nine-of-spades.png"), \
+("10 of Spades", 10, "ten-of-spades.png"), ("Jack of Spades", 10, "jack-of-spades.png"), \
+("Queen of Spades", 10, "queen-of-spades.png"), ("King of Spades", 10, "king-of-spades.png"), \
+("Ace of Diamonds", 11, "ace-of-diamonds.png"), ("2 of Diamonds", 2, "two-of-diamonds.png"), \
+("3 of Diamonds", 3, "three-of-diamonds.png"), ("4 of Diamonds", 4, "four-of-diamonds.png"), \
+("5 of Diamonds", 5, "five-of-diamonds.png"), ("6 of Diamonds", 6, "six-of-diamonds.png"), \
+("7 of Diamonds", 7, "seven-of-diamonds.png"), ("8 of Diamonds", 8, "eight-of-diamonds.png"), \
+("9 of Diamonds", 9, "nine-of-diamonds.png"), ("10 of Diamonds", 10, "ten-of-diamonds.png"), \
+("Jack of Diamonds", 10, "jack-of-diamonds.png"), ("Queen of Diamonds", 10, "queen-of-diamonds.png"), \
+("King of Diamonds", 10, "king-of-diamonds.png"), ("Ace of Clubs", 11, "ace-of-clubs.png"), \
+("2 of Clubs", 2, "two-of-clubs.png"), ("3 of Clubs", 3, "three-of-clubs.png"), \
+("4 of Clubs", 4, "four-of-clubs.png"), ("5 of Clubs", 5, "five-of-clubs.png"), \
+("6 of Clubs", 6, "six-of-clubs.png"), ("7 of Clubs", 7, "seven-of-clubs.png"), \
+("8 of Clubs", 8, "eight-of-clubs.png"), ("9 of Clubs", 9, "nine-of-clubs.png"), \
+("10 of Clubs", 10, "ten-of-clubs.png"), ("Jack of Clubs", 10, "jack-of-clubs.png"), \
+("Queen of Clubs", 10, "queen-of-clubs.png"), ("King of Clubs", 10, "king-of-clubs.png")]
---
+++ main.py
@@ -0,0 +1,7 @@
+import display
+
+if __name__ == '__main__':
+    print(display.logo)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
